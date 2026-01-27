3dc0fd007a0e4bb4d894ba4b3489e69e2614f48a
Joe Neeman <joe@neeman.me>
2026-01-26 19:42:17 -0600

    Git push to radicle by default

--- home-manager/jneeman.nix
+++ home-manager/jneeman.nix
@@ -174,6 +174,9 @@
       user.email = "joe@neeman.me";
       ui.default-command = "log";
       ui.pager = ":builtin";
+      git = {
+        fetch = ["origin" "rad"];
+      };
     };
   };
   programs.nix-index = {
