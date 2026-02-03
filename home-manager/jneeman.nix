7187f40c07d1ebde17e32d6887af0cf8005a08c5
Joe Neeman <joe@neeman.me>
2026-02-03 14:44:51 -0600

    add fish completer to nushell

--- home-manager/jneeman.nix
+++ home-manager/jneeman.nix
@@ -209,6 +209,30 @@
     settings = {
       show_banner = false;
     };
+    extraConfig = ''
+        let fish_completer = {|spans|
+          ${lib.getExe pkgs.fish} --command $'complete "--do-complete=($spans | str replace --all "'" "\\'" | str join " ")"'
+          | from tsv --flexible --noheaders --no-infer
+          | rename value description
+          | update value {|row|
+            let value = $row.value
+            let need_quote = ['\' ',' '[' ']' '(' ')' ' ' '\t' "'" '"' "`"] | any {$in in $value}
+            if ($need_quote and ($value | path exists)) {
+              let expanded_path = if ($value starts-with ~) {$value | path expand --no-symlink} else {$value}
+              $'"($expanded_path | str replace --all "\"" "\\\"")"'
+            } else {$value}
+          }
+        }
+
+        $env.config = {
+          completions: {
+            external: {
+              enable: true
+              completer: $fish_completer
+            }
+          }
+        }
+    '';
   };
 
   programs.eza = {
