36618e4528c3c0d619c87746ab8695a6b441278e
Joe Neeman <joe@neeman.me>
2026-04-18 15:49:45 -0500

    Add some tinymist config

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -4,19 +4,8 @@
   lib,
   ...
 }:
-let
-  asciidoc-ts = pkgs.fetchFromGitHub { owner = "cathaysia"; repo = "tree-sitter-asciidoc"; rev = "5b6ccea9953b939959f447f7bfea699ae6968a99"; hash = "sha256-mhBSe5umX2asVUwFWHvuVHQC33wOEy6s14dZWn3wAbQ="; };
-in
 
 {
-  # Whenever asciidoc-ts is updated, you need to manually remove the old object files, as hx won't rebuild it due to Nix's epoch timestamps.
-  # $ rm ~/.config/helix/runtime/grammars/asciidoc*.so
-  # It might be worth switching to git sources that hx can fetch/manage instead of path sources under the control of the flake for this reason.
-  home.activation.hx-grammar-build = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    export PATH="${pkgs.stdenv.cc}/bin:${config.programs.helix.package}/bin:${config.programs.git.package}/bin:$PATH"
-    run --quiet hx --grammar fetch # Only necessary for git URL based sources
-    run --quiet hx --grammar build
-  '';
   programs.helix = {
     enable = true;
     settings = {
@@ -42,17 +31,6 @@ in
       };
     };
     languages = {
-      grammar = [
-        {
-          name = "asciidoc";
-          source.path = "${asciidoc-ts}/tree-sitter-asciidoc";
-        }
-        {
-          name = "asciidoc_inline";
-          source.path = "${asciidoc-ts}/tree-sitter-asciidoc_inline";
-        }
-      ];
-
       language = [
         {
           name = "rust";
@@ -71,25 +49,9 @@ in
           auto-format = true;
         }
         {
-          name = "asciidoc";
-          language-id = "asciidoc";
-          scope = "source.adoc";
-          injection-regex = "adoc";
-          file-types = [ "adoc" ];
-          comment-tokens = [ "//" ];
-          block-comment-tokens = [{
-            start = "////";
-            end = "////";
-          }];
-          grammar = "asciidoc";
-        }
-        {
-          name = "asciidoc_inline";
-          language-id = "asciidoc_inline";
-          scope = "source.asciidoc_inline";
-          injection-regex = "asciidoc_inline";
-          file-types = [];
-          grammar = "asciidoc_inline";
+          name = "typst";
+          file-types = [ "typ" ];
+          language-servers = [ "tinymist" ];
         }
       ];
 
@@ -103,9 +65,13 @@ in
         };
         files.watcher = "server";
       };
+
+      language-server.tinymist = {
+        command = "tinymist";
+        config = {
+          preview.background.enabled = true;
+        };
+      };
     };
   };
-
-  xdg.configFile."helix/runtime/queries/asciidoc".source = "${asciidoc-ts}/queries/asciidoc";
-  xdg.configFile."helix/runtime/queries/asciidoc_inline".source = "${asciidoc-ts}/queries/asciidoc_inline";
 }
